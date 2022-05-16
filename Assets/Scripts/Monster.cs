912fd84a26df973d55947b76286fdcf52373a974
Jorge Celaya <jcelaya775@gmail.com>
2022-05-16 14:32:18 -0500

    added  project files

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed;
+
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        body.velocity = new Vector2(speed, body.velocity.y);
+    }
+}
